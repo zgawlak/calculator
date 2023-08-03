0a80eb664b4625d6bcf10c7ce03706039b5cb323
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-08-03 10:43:34 -0400

    Create number button and operator button handlers

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
 let currentValue = "";
+let prevValue = "";
 
 let currentCalc = document.querySelector('.current-calc');
+let prevCalc = document.querySelector('.prev-calc');
 let numberButtons = document.querySelectorAll('.num-buttons');
+let operatorButtons = document.querySelectorAll('.op-buttons');
+let equalsButton = document.querySelector('.equals-button');
 
 function add(a, b) {
     return a + b;
@@ -39,13 +43,34 @@ function operate(first, second) {
             divide(first, second);
             break;
     }
-}
+} ``
 
-function updateDisplayValue(e) {
+function handleNumberBtn(e) {
     currentValue += e.target.textContent;
     currentCalc.textContent = currentValue;
 }
 
+function handleOperatorBtn(e) {
+    if (!prevValue) {
+        prevValue += currentValue;
+        prevValue += " " + operator;
+        prevCalc.textContent = prevValue;
+        firstNumber = Number(currentValue);
+    }
+    else {
+        prevValue = currentValue + " " + operator;
+        prevCalc.textContent = prevValue;
+    }
+}
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', updateOperator);
+});
+
 numberButtons.forEach((button) => {
-    button.addEventListener('click', updateDisplayValue);
+    button.addEventListener('click', handleNumberBtn);
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', handleOperatorBtn);
 });
